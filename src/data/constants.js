e754733f6ecc5011c3b926ae1832b0cba6c85c60
S.Barath Viswa Raj <135594826+Barath-viswa-raj@users.noreply.github.com>
2025-01-17 23:57:25 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,393 @@
+export const Bio = {
+    name: "Barth Viswa Raj S",
+    roles: [
+      "Data Scientist",
+      "ML Engineer",
+      "Front-End Developer",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/Barath-viswa-raj",
+    resume:
+      "https://drive.google.com/file/d/1zd1-kjKhztP2Vpc-E9o1_iDKLTxVU2u6/view?usp=drive_link",
+    linkedin: "https://www.linkedin.com/in/barath-viswa-raj-12a883259/",
+    twitter: "https://x.com/sbarathviswaraj",
+    insta: "https://www.instagram.com/rishav_chanda/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+
+        {
+          name: "React",
+          image:
+            "https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg",
+        },
+
+
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        
+       
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+        {
+          name: "Firebase",
+          image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+        },
+        {
+          name: "Supabase",
+          image: "https://www.vectorlogo.zone/logos/supabase/supabase-icon.svg",
+        },
+      ],
+    },
+    {
+      title:"Data Science and Machine Learning Language",
+      skills: [
+        {
+          name: "Numpy",
+          image: "https://www.vectorlogo.zone/logos/numpy/numpy-ar21.svg",
+        },
+
+        {
+          name: "Pytorch",
+          image: "https://www.vectorlogo.zone/logos/pytorch/pytorch-icon.svg",
+        },
+
+        {
+          name: "Tensorflow",
+          image: "https://www.vectorlogo.zone/logos/tensorflow/tensorflow-icon.svg",
+        },
+
+
+        {
+          name: "Jupyter",
+          image: "https://www.vectorlogo.zone/logos/jupyter/jupyter-icon.svg",
+        },
+
+        {
+          name: "Flask ",
+          image: "https://www.vectorlogo.zone/logos/palletsprojects_flask/palletsprojects_flask-icon~v2.svg",
+        },
+
+        {
+          name: "OpenCV ",
+          image: "https://www.vectorlogo.zone/logos/opencv/opencv-icon.svg",
+        },
+
+        {
+          name: "OpenCV ",
+          image: "https://www.vectorlogo.zone/logos/opencv/opencv-icon.svg",
+        },
+
+
+
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Docker",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+        },
+        
+        
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+       
+      
+        {
+          name: "Figma",
+          image:
+            "https://www.vectorlogo.zone/logos/figma/figma-icon.svg",
+        },
+
+        {
+          name: "Powerbi",
+          image:
+            "https://www.vectorlogo.zone/logos/microsoft_powerbi/microsoft_powerbi-icon.svg",
+        },
+
+        {
+          name: "Tableau",
+          image:
+            "https://jbboyyrlbimyjeuzjckf.supabase.co/storage/v1/object/sign/portfolio/tableau-icon.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwb3J0Zm9saW8vdGFibGVhdS1pY29uLnN2ZyIsImlhdCI6MTczNzEwMzY3NiwiZXhwIjoyMzY3ODIzNjc2fQ.oxh55yfJ7rMCHBTI2IWbkXsbKzq1zm2SRXlT5ajBnDs&t=2025-01-17T08%3A47%3A56.919Z",
+        },
+        
+        {
+          name: "Visual Code",
+          image:
+           "https://www.vectorlogo.zone/logos/visualstudio_code/visualstudio_code-ar21.svg",
+        },
+        
+        {
+          name: "Canvas",
+          image:
+            "https://www.vectorlogo.zone/logos/canva/canva-ar21.svg",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      role: "React Intern",
+      company: "RV Tech Pvt. Ltd.",
+      date: "Feb 2024 - Feb 2024",
+      desc: "Creating the real time project .The project named Social media dashboard has created " ,
+      skills: [
+        "HTML",
+        "CSS",
+        "JAVASCRIPT",
+        "TAILWIND CSS",
+        "FIGMA",
+        "REACT JS",
+      ],
+      doc: "",
+    },
+    {
+      id: 0,
+      role: "Data Science Intern",
+      company: "Intelcraft Infotech",
+      date: "June 2024 - June 2024 ",
+      desc: "Working on the Real time data and create a model(machine learning) with the front end application.",
+      skills: [
+        "Seaborn",
+        "Matplotlib",
+        "Numpy",
+        "python-radio",
+        "Python",
+        "HTML",
+        "CSS",
+        "JavaScript",
+      ],
+      doc: "",
+    },
+    {
+      id: 1,
+      role: "Data Science Intern",
+      company: "Corizo",
+      date: "September 2024 - November 2024",
+      desc: "A 3 month internship with the one month of industry studies and the two projects (major and minor project) developed with the frontend .",
+      skills: [
+        "ReactJS",
+        "Python",
+        "Numpy",
+        "Pandas",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "JAVASCPRIT",
+      ],
+    },
+    {
+      id: 2,
+      role: "DATA SCIENCE INTERN",
+      company: "ZIDIO DEVELOPMENT PVT.LMT",
+      date: "SEPTEMBER 2024 - DECEMBER 2024",
+      desc: "LEARN FORM THE INDUSTRY EXPECT AND DEVELOPED THE TWO PROJECT ON THE DURATION OF 2 MONTHS ",
+      skills: [
+        "ReactJS",
+        "Python",
+        "Numpy",
+        "Pandas",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "OPENCV",
+        "YOLO",
+        "HUGGINGFACE",
+        "JAVASCPRIT",
+      ],
+      doc: "",
+    },
+    
+    ];
+
+  export const education = [
+    {
+      id: 0,
+      img: "https://jbboyyrlbimyjeuzjckf.supabase.co/storage/v1/object/sign/portfolio/college.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwb3J0Zm9saW8vY29sbGVnZS5wbmciLCJpYXQiOjE3MzcxMDQyOTcsImV4cCI6MTczODgzMjI5N30.1rIoHid9rseXLcLRVI07oRAEm4mqR-8c1dYz8veWom8&t=2025-01-17T08%3A58%3A17.654Z",
+      school: "Sri Eshwar Engineering of college,Coimbatore" ,
+      date: "Nov 2022 - Sep 2026",
+      grade: "7.79 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering(Artifical Intelligence and Machine Learning) at Sri Eshwar Engineering of College, Coimbatore. I have completed 3 semesters and have a CGPA of 7.79. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others, where I am learning and working on exciting projects with a team of talented developers.",
+      degree: "Bachelor of Technology - BTech, Computer Science and Engineering(Artifical Intelligence and Machine Learning) at Coimbatore",
+    },
+    {
+      id: 1,
+      img: "https://jbboyyrlbimyjeuzjckf.supabase.co/storage/v1/object/sign/portfolio/SCHOOL.jpeg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwb3J0Zm9saW8vU0NIT09MLmpwZWciLCJpYXQiOjE3MzcxMDM1MDQsImV4cCI6MjM2NzgyMzUwNH0.l52D7KVdh7-cJpGsuY5uEGbMaJoVgzpZmgU124NtbL0&t=2025-01-17T08%3A45%3A05.079Z",
+      school: "Francis Xavier School (CBSE),Tirunelveli",
+      date: "2020-2022",
+      grade: "65.9%",
+      desc: "I completed my class 12 high school education at Francis Xavier School,Tirunelveli where I studied  biology and mathematics.",
+      degree: "Higher Education on biology and mathematics",
+    },
+    {
+      id: 2,
+      img: "https://jbboyyrlbimyjeuzjckf.supabase.co/storage/v1/object/sign/portfolio/SCHOOL.jpeg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwb3J0Zm9saW8vU0NIT09MLmpwZWciLCJpYXQiOjE3MzcxMDM1MDQsImV4cCI6MjM2NzgyMzUwNH0.l52D7KVdh7-cJpGsuY5uEGbMaJoVgzpZmgU124NtbL0&t=2025-01-17T08%3A45%3A05.079Z",
+      school: "Francis Xavier School (CBSE),Tirunelveli",
+      date: "2020",
+      grade: "67.8%",
+      desc: "I completed my class 10 education at Francis Xavier School,Tirunelveli where I studied class 1 - 10 .",
+      degree: "Higher Secondary Education ",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "Federated-for-health",
+      date: "Jun 2024 - Jul 2024",
+      description:
+        "While rich medical datasets are hosted in hospitals distributed across the world, concerns on patients' privacy is a barrier against using such data to train deep neural networks (DNNs) for medical diagnostics. We propose Dopamine, a system to train DNNs on distributed datasets, which employs federated learning (FL) with differentially-private stochastic gradient descent (DPSGD), and, in combination with secure aggregation, can establish a better trade-off between differential privacy (DP) guarantee and DNN's accuracy than other approaches. Results on a diabetic retinopathy~(DR) task show that Dopamine provides a DP guarantee close to the centralized training counterpart, while achieving a better classification accuracy than FL with parallel DP where DPSGD is applied without coordination.",
+      image:
+        "https://jbboyyrlbimyjeuzjckf.supabase.co/storage/v1/object/sign/portfolio/Screenshot%202024-10-09%20142531.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwb3J0Zm9saW8vU2NyZWVuc2hvdCAyMDI0LTEwLTA5IDE0MjUzMS5wbmciLCJpYXQiOjE3MzcxMzU2MzQsImV4cCI6MjM2Nzg1NTYzNH0.EL0Rww1-5spd8tFpLsKNkBO3L7YtiMhISpsiAaqoSwk&t=2025-01-17T17%3A40%3A34.009Z",
+      tags: [
+        "Docker",
+        "HTML",
+        "CSS",
+        "JAVASCRIPT",
+        "Federated Learning",
+        "Differentially-Private stochastic gradient descent(DPSGD)",
+        "Machine learning",
+            ],
+      category: "web app",
+      github: "https://github.com/Barath-viswa-raj/Federated-for-health",
+    },
+    {
+      id: 0,
+      title: "Social Media Dashboard",
+      date: "Apr 2024 - May 2024",
+      description:
+        "A social media dashboard is a centralized platform designed to manage, monitor, and analyze social media activities across various networks. Here are some key components and features typically included in a social media dashboard",
+      image:
+        "https://jbboyyrlbimyjeuzjckf.supabase.co/storage/v1/object/sign/portfolio/social.avif?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwb3J0Zm9saW8vc29jaWFsLmF2aWYiLCJpYXQiOjE3MzcxMzU4NTYsImV4cCI6MjM2Nzg1NTg1Nn0.6FMkYDy0S2xsc6gusGkR80dVtHfosVRkvdqgHyBwFcE&t=2025-01-17T17%3A44%3A16.244Z",
+      tags: ["React Js", "TAILWIND CSS", "HTML", "JAVASCPRIT", ],
+      category: "web app",
+      github: "https://github.com/Barath-viswa-raj/social-media",
+    },
+    {
+      id: 2,
+      title: "AI-ARTISITIC-COLLABORATIVE-WEBPAGE",
+      date: "Jan 2024 - Mar 2024",
+      description:
+        "The AI Artistic Collaboration Platform is designed to bridge the gap between artists and artificial intelligence, providing a collaborative space for creating innovative artworks. This platform leverages AI to enhance creativity, enabling artists to experiment with new styles and techniques, and offering tools that streamline the creative process.",
+      image:
+        "https://jbboyyrlbimyjeuzjckf.supabase.co/storage/v1/object/sign/portfolio/1.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwb3J0Zm9saW8vMS5wbmciLCJpYXQiOjE3MzcxMzYyNTQsImV4cCI6MjM2Nzg1NjI1NH0.ERffKYiyf_0EX7m6jqnBNddeqdZ5J6gCG93vF_WIdS0&t=2025-01-17T17%3A50%3A54.806Z",
+      tags: ["Python", "HugggingFace", "TensorFlow", "GoggleGemini", "Pickle", "React","Flasks"],
+      category: "machine learning",
+      github: "https://github.com/Barath-viswa-raj/AI-Artistic-Collaborative-Webpage",
+    },
+    {
+      id: 3,
+      title: "Music-recommendation ",
+      date: "Dec 2024 - Apr 2024",
+      description:
+        "This document explores music recommendation, offering a diverse selection across genres like pop, rock, hip-hop, jazz, and electronic. It highlights tracks such as Dua Lipa's Levitating, Foo Fighters' Everlong,  and Miles Davis' So What,  promoting genre exploration to enrich listening experiences and foster cultural appreciation.",
+      image:
+        "https://jbboyyrlbimyjeuzjckf.supabase.co/storage/v1/object/sign/portfolio/music.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwb3J0Zm9saW8vbXVzaWMucG5nIiwiaWF0IjoxNzM3MTM2NTU4LCJleHAiOjIzNjc4NTY1NTh9.sajAZNcEiWyM8dNZOhQUPmIRNacDQJ5LJ6KezDmg--k&t=2025-01-17T17%3A55%3A58.477Z",
+      tags: ["Machine learning", "Html", "Css", "Javascprit","spotify","pandas","numpy"],
+      category: "android app",
+      github: "https://github.com/Barath-viswa-raj/Music-recommendation",
+          },
+    {
+      id: 10,
+      title: "Intern Project",
+      date: "Sep 2024 - nov 2024",
+      description:
+        "During my paid internship at Corizo, in collaboration with Wipro, I was thrilled to dive deep into the fascinating world of data science. This experience was not only challenging but also incredibly rewarding. I worked on two major projects that really pushed my boundaries and helped me grow.",
+      image:
+        "https://jbboyyrlbimyjeuzjckf.supabase.co/storage/v1/object/sign/portfolio/corizo.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwb3J0Zm9saW8vY29yaXpvLmpwZyIsImlhdCI6MTczNzEzNjkzOCwiZXhwIjoyMzY3ODU2OTM4fQ.S0AaolJeemv1Upi0FqpMjFVOkBQ0eK14kDK2FR1IpPE&t=2025-01-17T18%3A02%3A18.940Z",
+      tags: ["Machine learning", "JavaScript", "Html","gradio","Pandas","Numpy"],
+      category: "web app",
+      github: "https://github.com/Barath-viswa-raj/Corizo_project",
+          },
+   
+    {
+      id: 5,
+      title: "Todo Web App",
+      date: "Jun 2024",
+      description:
+        " A Todo Web App made with React JS. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
+      image:
+        "https://jbboyyrlbimyjeuzjckf.supabase.co/storage/v1/object/sign/portfolio/to%20do%20list.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwb3J0Zm9saW8vdG8gZG8gbGlzdC5qcGciLCJpYXQiOjE3MzcxMzcyMDAsImV4cCI6MjM2Nzg1NzIwMH0.JD7DpgQrbRa4Y7Sp8xPsyr8FqadI3N_hec1KZ7dW7d0&t=2025-01-17T18%3A06%3A40.879Z",
+      tags: ["React Js", "Html","Css","JavaScript"],
+      category: "web app",
+      github: "https://github.com/Barath-viswa-raj/TO-DO-List-React-",
+    },
+   
+    
+  ];
+  
+  export const TimeLineData = [
+    { year: 2020, text: "Started my journey" },
+    { year: 2022, text: "Worked as a freelance developer" },
+    { year: 2023, text: "Founded UI-UX Design" },
+    { year: 2023, text: "Shared my projects with the world" },
+    { year: 2024, text: "Started my own platform" },
+  ];
